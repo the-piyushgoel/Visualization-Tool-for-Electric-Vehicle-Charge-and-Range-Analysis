ead3ffccbbe91fa74d6409ff7505d9e1afdfe74e
Piyush Goel <piyush.goel2704@gmail.com>
2026-03-08 22:17:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='', static_folder='')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboards')
+def dashboards():
+    return render_template('page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
